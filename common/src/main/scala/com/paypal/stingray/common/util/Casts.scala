fdc38dfa07a9912b99950e518cda55a8f10a3128
Andrew Harris <awharris@paypal.com>
2014-01-23 13:54:45 -0800

    convert Casts to use ClassTag

--- common/src/main/scala/com/paypal/stingray/common/util/Casts.scala
+++ common/src/main/scala/com/paypal/stingray/common/util/Casts.scala
@@ -1,52 +1,65 @@
 package com.paypal.stingray.common.util
 
-import scala.collection.mutable.ListBuffer
+import scala.reflect._
 
 /*
- * Contains utility classes for class casting. Examples:
+ * Utility classes for class casting.
  *
- * import com.paypal.stingray.util.casts._
- * ...
- * // `.cast[T]` casts an object to type T, returning an Option[T] which is some iff the object may be cast, none otherwise
- * def castToString(obj: Obj): String =
- *   obj.cast[String] | (throw new ClassCastException("not a String")
+ * {{{
+ *    import com.paypal.stingray.util.casts._
  *
- * // `.cast[T]` on an Option[A] returns an Option[T] which is Some[T] containing the casted contents of the original
- * // option iff those contents may be cast to a T, None otherwise.
- * def castOptionToString(opt: Option[_]): String =
- *   opt.cast[String] | (throw new ClassCastException("not a String"))
+ *   // `.cast[T]` casts an object to type T, returning an Option[T] which is some iff the object may be cast, none otherwise
+ *   def castToString(obj: Obj): String =
+ *     obj.cast[String].getOrElse(throw new ClassCastException("not a String")
  *
- * // `.cast[T]` on an Array[A] returns a Array[T] containing only the elements of the original array
- * // which may be cast to a T
- * def findStringsInArray(arr: Array[_]): Array[String] =
- *   arr.cast[String]
+ *   // `.cast[T]` on an Option[A] returns an Option[T] which is Some[T] containing the casted contents of the original
+ *   // option iff those contents may be cast to a T, None otherwise.
+ *   def castOptionToString(opt: Option[_]): String =
+ *     opt.cast[String].getOrElse(throw new ClassCastException("not a String"))
  *
- * // `.cast[T]` on a Traversable[A] returns a Traversable[T] containing only the elements of the original traverable
- * // which may be cast to a T
- * def findStringsInTraversable(tr: Traversable[_]): Traversable[String] =
- *   tr.cast[String]
+ *   // `.cast[T]` on an Array[A] returns a Array[T] containing only the elements of the original array
+ *   // which may be cast to a T
+ *   def findStringsInArray(arr: Array[_]): Array[String] =
+ *     arr.cast[String]
  *
- * // `.cast[T]` on a List[A] returns a List[T] containing only the elements of the original list
- * // which may be cast to a T
- * def findStringsInList(li: List[_]): List[String] =
- *   li.cast[String]
+ *   // `.cast[T]` on a Traversable[A] returns a Traversable[T] containing only the elements of the original traverable
+ *   // which may be cast to a T
+ *   def findStringsInTraversable(tr: Traversable[_]): Traversable[String] =
+ *     tr.cast[String]
+ *
+ *   // `.cast[T]` on a List[A] returns a List[T] containing only the elements of the original list
+ *   // which may be cast to a T
+ *   def findStringsInList(li: List[_]): List[String] =
+ *     li.cast[String]
+ * }}}
  */
 
 object casts {
 
+  /**
+   * Wrapper to allow any class to be cast, based on a Manifest for that class
+   * @param any the class to be cast
+   */
   implicit class CastableAny(any: Any) {
-    def cast[T](implicit target: Manifest[T]): Option[T] = {
+
+    /**
+     * Cast the wrapped object as type `T`, optionally returning the newly cast object if the cast was successful
+     * @param target the manifest of type `T`
+     * @tparam T the type to which this wrapped object will be cast
+     * @return optionally, the newly cast object, or None if the cast was unsuccessful
+     */
+    def cast[T](implicit target: ClassTag[T]): Option[T] = {
       Option(any).flatMap { _ =>
         val source = any match {
-          case _: Boolean => manifest[Boolean]
-          case _: Byte => manifest[Byte]
-          case _: Char => manifest[Char]
-          case _: Short => manifest[Short]
-          case _: Int => manifest[Int]
-          case _: Long => manifest[Long]
-          case _: Float => manifest[Float]
-          case _: Double => manifest[Double]
-          case _ => Manifest.classType(any.getClass)
+          case _: Boolean => classTag[Boolean]
+          case _: Byte => classTag[Byte]
+          case _: Char => classTag[Char]
+          case _: Short => classTag[Short]
+          case _: Int => classTag[Int]
+          case _: Long => classTag[Long]
+          case _: Float => classTag[Float]
+          case _: Double => classTag[Double]
+          case _ => ClassTag(any.getClass)
         }
         if (target.runtimeClass.isAssignableFrom(source.runtimeClass)) {
           Some(any.asInstanceOf[T])
@@ -61,37 +74,37 @@ object casts {
   }
 
   implicit class CastableOption(opt: Option[_]) {
-    def cast[T : Manifest]: Option[T] = {
+    def cast[T : ClassTag]: Option[T] = {
       opt.flatMap(_.cast[T])
     }
-    def castIf[T : Manifest](pred: T => Boolean): Option[T] = {
+    def castIf[T : ClassTag](pred: T => Boolean): Option[T] = {
       cast[T].flatMap(c => if(pred(c)) Some(c) else None)
     }
   }
 
   implicit class CastableTraversable(traversable: Traversable[_]) {
-    def cast[T : Manifest]: Traversable[T] = {
+    def cast[T : ClassTag]: Traversable[T] = {
       traversable.flatMap(_.cast[T])
     }
-    def castIf[T : Manifest](pred: T => Boolean): Traversable[T] = {
+    def castIf[T : ClassTag](pred: T => Boolean): Traversable[T] = {
       cast[T].flatMap(c => if(pred(c)) Some(c) else None)
     }
   }
 
   implicit class CastableList(list: List[_]) {
-    def cast[T : Manifest]: List[T] = {
+    def cast[T : ClassTag]: List[T] = {
       list.flatMap(_.cast[T])
     }
-    def castIf[T : Manifest](pred: T => Boolean): List[T] = {
+    def castIf[T : ClassTag](pred: T => Boolean): List[T] = {
       cast[T].flatMap(c => if(pred(c)) Some(c) else None)
     }
   }
 
   implicit class CastableArray(array: Array[_]) {
-    def cast[T : Manifest]: Array[T] = {
+    def cast[T : ClassTag]: Array[T] = {
       array.flatMap(_.cast[T])
     }
-    def castIf[T : Manifest](pred: T => Boolean): Array[T] = {
+    def castIf[T : ClassTag](pred: T => Boolean): Array[T] = {
       cast[T].flatMap(c => if(pred(c)) Some(c) else None)
     }
   }
