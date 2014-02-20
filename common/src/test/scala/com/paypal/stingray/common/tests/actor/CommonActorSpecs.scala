dad6e15afa19ddff248809f0108d9cf827c65f35
Matt Vaznaian <mvaznaian@paypal.com>
2014-02-20 15:54:55 -0800

    Common/Service actor specs

--- common/src/test/scala/com/paypal/stingray/common/tests/actor/CommonActorSpecs.scala
+++ common/src/test/scala/com/paypal/stingray/common/tests/actor/CommonActorSpecs.scala
@@ -1,21 +1,46 @@
 package com.paypal.stingray.common.tests.actor
 
 import org.specs2._
-import com.paypal.stingray.common.tests.actor.ActorSpecification
-import com.paypal.stingray.common.actor.CommonActor
-import akka.actor.ActorSystem
-import akka.actor.Actor
-import akka.actor.Props
-import akka.testkit.TestKit
+import akka.pattern.ask
+import com.paypal.stingray.common.actor._
+import akka.actor._
+import akka.testkit._
+import com.paypal.stingray.common.tests.util.CommonImmutableSpecificationContext
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
 
 /**
  * Tests [[com.paypal.stingray.common.actor.CommonActor]]
  */
-class CommonActorSpecs extends Specification { def is = s2"""
+class CommonActorSpecs
+  extends TestKit(ActorSystem("CommonActorSpecs"))
+  with SpecificationLike
+  with ActorSpecification
+  with ScalaCheck { def is = s2"""
 
-  // Working on
-
+  Passing an unhandled exception results in an UnhandledMessageException ${Message().failureCase}
 
 """
+
+  trait Context
+    extends CommonImmutableSpecificationContext {
+
+    class TestActor extends ServiceActor {
+      def receive = {
+        case num: Int =>
+      }
+    }
+
+    lazy val testActor: ActorRef = TestActorRef(new TestActor)
+    implicit lazy val timeout = Timeout(10, TimeUnit.SECONDS)
+  }
+
+  case class Message() extends Context {
+
+    def failureCase = this {
+      (testActor ? "hello").mapTo[String].toTry must beFailedTry[String].withThrowable[UnhandledMessageException]
+    }
+
+  }
 
 }
