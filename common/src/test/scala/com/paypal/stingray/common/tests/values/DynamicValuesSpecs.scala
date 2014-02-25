21e7b7d3183510deabe0bbc229e47a347833f9b5
Matt Vaznaian <mvaznaian@paypal.com>
2014-02-24 16:47:39 -0800

    Add more Static/Dynamic values specs, coverage at 85%

--- common/src/test/scala/com/paypal/stingray/common/tests/values/DynamicValuesSpecs.scala
+++ common/src/test/scala/com/paypal/stingray/common/tests/values/DynamicValuesSpecs.scala
@@ -6,29 +6,32 @@ import akka.testkit._
 import akka.actor._
 import scala.concurrent._
 import com.paypal.stingray.common.tests.actor.ActorSpecification
+import org.specs2.mock.Mockito
 
 /**
  * Tests [[com.paypal.stingray.common.values.DynamicValues]]
  */
-class DynamicValuesSpecs extends TestKit(ActorSystem()) with ActorSpecification with SpecificationLike { def is = s2"""
+class DynamicValuesSpecs extends TestKit(ActorSystem()) with ActorSpecification with SpecificationLike with Mockito { def is = s2"""
 
-  Retrieving dynamic value asyncronously works ${GetAsync().ok}
-  Failed retrieval looks in static values file
-  If key in neither file, ...
-  getAll
-  get ${Get().ok}
+  Retrieving dynamic value asyncronously works      ${GetAsync().successCase}
+  Failed dv lookup defaults to static values file   ${GetAsync().defaultToSV}
+  When attempt fails, returns exception             ${GetAsync().failureCase}
+  getAll                                            ${GetAll().ok}
+  get                                               ${Get().ok}
 
 """
 
-  class ExtendedDV extends DynamicValues(None) {
+  private val defaultAttempt = { key: String =>
+    Future.successful {
+      Some(key)
+    }
+  }
+  private class DummyDV(svs: StaticValues = StaticValues.defaultValues)
+                        (attemptFn: String => Future[Option[String]] = defaultAttempt) extends DynamicValues(svs) {
 
     override implicit val seqCtx = synchronousExecution
 
-    override def attempt(key: String): Future[Option[String]] = {
-      Future.successful {
-        Some(key)
-      }
-    }
+    override def attempt(key: String) = attemptFn(key)
 
     override def getAll: Future[Map[String, String]] = {
       Future.successful {
@@ -39,16 +42,40 @@ class DynamicValuesSpecs extends TestKit(ActorSystem()) with ActorSpecification 
   }
 
   case class GetAsync() {
-    def ok = {
-      val dv = new ExtendedDV
+    def successCase = {
+      val dv = new DummyDV()()
       val value = dv.getAsync("anything").mapTo[Option[String]].toTry
       value must beSuccessfulTry[Option[String]].withValue(Some("anything"))
+    }
+    def defaultToSV = {
+      val url = getClass.getClassLoader.getResource("test.properties")
+      val sv = new StaticValues(url)
+      val dv = new DummyDV(sv)({ key: String =>
+        Future.successful(Option.empty[String])
+      })
+      val value = dv.getAsync("some.property").mapTo[Option[String]].toTry
+      value must beSuccessfulTry[Option[String]].withValue(Some("somevalue"))
+    }
+    def failureCase = {
+      val dv = new DummyDV()({ key: String =>
+        Future.failed { new Exception("foo") }
+      })
+      val value = dv.getAsync("some.property").mapTo[Option[String]].toTry
+      value must beFailedTry[Option[String]].withThrowable[Exception]("foo")
+    }
+  }
+
+  case class GetAll() {
+    def ok = {
+      val dv = new DummyDV()()
+      val value = dv.getAll.mapTo[Map[String, String]].toTry
+      value must beSuccessfulTry[Map[String, String]]
     }
   }
 
   case class Get() {
     def ok = {
-      val dv = new ExtendedDV
+      val dv = new DummyDV()()
       val value = dv.get("anything")
       value must beSome[String].like {
         case v => v must beEqualTo("anything")
--- common/src/test/scala/com/paypal/stingray/common/tests/values/StaticValuesSpecs.scala
+++ common/src/test/scala/com/paypal/stingray/common/tests/values/StaticValuesSpecs.scala
@@ -3,6 +3,7 @@ package com.paypal.stingray.common.tests.values
 import org.specs2._
 import com.paypal.stingray.common.values.StaticValues
 import com.paypal.stingray.common.tests.util.CommonImmutableSpecificationContext
+import com.paypal.stingray.common.enumeration._
 
 /**
  * Tests [[com.paypal.stingray.common.values.StaticValues]]
@@ -12,20 +13,23 @@ class StaticValuesSpecs extends Specification with ScalaCheck { def is = s2"""
   StaticValues is an implementation of com.paypal.stingray.common.values.Values
   to read synchronously from local properties files.
 
-  Constructors:
-    Init with a root resource                                               ${Constructors.ByRootResource().ok}
-    Init with serviceName should cause a lookup for serviceName.config      ${Constructors.ByServiceName().ok}
-    Init with a URL should return configured StaticValue instance           ${Constructors.ByURL().ok}
-    Init with no service name attempts to load from service.cluster.config  ${Constructors.NoParams().ok}
-    Using defaultValues should return parameterless instance                ${Constructors.DefaultValues().ok}
-
   Get Methods:
     getOrDie should return value when property key exists             ${GetMethods.GetOrDie().successCase}
     getOrDie should throw when property key does not exist            ${GetMethods.GetOrDie().failureCase}
+
     getIntOrDie should return int value when property key exists      ${GetMethods.GetIntOrDie().successCase}
-    getIntOrDie should throw when property key does not exist         ${GetMethods.GetIntOrDie().failureCase}
+    getIntOrDie should throw when value is incorrect type             ${GetMethods.GetIntOrDie().failureCase}
+
     getLongOrDie should return long value when property key exists    ${GetMethods.GetLongOrDie().successCase}
-    getLongOrDie should throw when property key does not exist        ${GetMethods.GetLongOrDie().failureCase}
+    getLongOrDie should throw when value is incorrect type            ${GetMethods.GetLongOrDie().failureCase}
+
+    getBool should return boolean value when property key exists      ${GetMethods.GetBool().successCase}
+    getBool should return None when value is incorrect type           ${GetMethods.GetBool().failureCase}
+
+    getEnum should return enum value when property key exists         ${GetMethods.GetEnum().successCase}
+    getEnum should return None when value is incorrect type           ${GetMethods.GetEnum().failureCase}
+
+    getSimpleList should return value when property key exists        ${GetMethods.GetSimpleList().successCase}
 
 """
 
@@ -36,70 +40,6 @@ class StaticValuesSpecs extends Specification with ScalaCheck { def is = s2"""
   trait GetContext extends CommonImmutableSpecificationContext {
     val url = getClass.getClassLoader.getResource("test.properties")
     val values = new StaticValues(url)
-  }
-
-  object Constructors {
-
-    case class ByRootResource() extends ConstructorsContext {
-      def ok = this {
-        val values = new StaticValues("test")
-        val property = values.get("some.property")
-        property must beEqualTo(Some("somevalue"))
-      }
-    }
-
-    case class ByServiceName() extends ConstructorsContext {
-      override def before {
-        System.setProperty("some-service.config", "src/test/resources/test.properties")
-      }
-      override def after {
-        System.clearProperty("some-service.config")
-      }
-      def ok = this {
-
-        val values = new StaticValues("some-service")
-        val property = values.get("some.property")
-        property must beEqualTo(Some("somevalue"))
-      }
-    }
-
-    case class ByURL() extends ConstructorsContext {
-      def ok = this {
-        val url = getClass.getClassLoader.getResource("test.properties")
-        val values = new StaticValues(url)
-        val property = values.get("some.property")
-        property must beEqualTo(Some("somevalue"))
-      }
-    }
-
-    case class NoParams() extends ConstructorsContext {
-      override def before {
-        System.setProperty("stingray.cluster.config", "src/test/resources/test.properties")
-      }
-      override def after {
-        System.clearProperty("stingray.cluster.config")
-      }
-      def ok = this {
-        val values = new StaticValues()
-        val property = values.get("some.property")
-        property must beEqualTo(Some("somevalue"))
-      }
-    }
-
-    case class DefaultValues() extends ConstructorsContext {
-      override def before {
-        System.setProperty("stingray.cluster.config", "src/test/resources/test.properties")
-      }
-      override def after {
-        System.clearProperty("stingray.cluster.config")
-      }
-      def ok = this {
-        val values = StaticValues.defaultValues
-        val property = values.get("some.property")
-        property must beEqualTo(Some("somevalue"))
-      }
-    }
-
   }
 
   object GetMethods {
@@ -134,5 +74,53 @@ class StaticValuesSpecs extends Specification with ScalaCheck { def is = s2"""
       }
     }
 
+    case class GetBool() extends GetContext {
+      def successCase = this {
+        val property = values.getBool("some.bool")
+        property must beEqualTo(Some(true))
+      }
+      def failureCase = this {
+        val property = values.getBool("some.not.bool")
+        property must beEqualTo(None)
+      }
+    }
+
+    case class GetEnum() extends GetContext {
+      sealed abstract class EnvironmentType extends Enumeration
+      object EnvironmentType extends EnumUnapply[EnvironmentType] {
+
+        object DEV extends EnvironmentType {
+          val stringVal = "dev"
+        }
+        object PROD extends EnvironmentType {
+          val stringVal = "prod"
+        }
+
+        implicit val environmentTypeRead: EnumReader[EnvironmentType] = new EnumReader[EnvironmentType] {
+          override def read(s: String): Option[EnvironmentType] = s.toLowerCase match {
+            case DEV.stringVal => Some(DEV)
+            case PROD.stringVal => Some(PROD)
+            case _ => None
+          }
+        }
+
+      }
+      def successCase = this {
+        val property = values.getEnum[EnvironmentType]("some.enum")
+        property must beEqualTo(Some(EnvironmentType.DEV))
+      }
+      def failureCase = this {
+        val property = values.getEnum[EnvironmentType]("some.bool")
+        property must beEqualTo(None)
+      }
+    }
+
+    case class GetSimpleList() extends GetContext {
+
+      def successCase = this {
+        val property = values.getSimpleList("some.list")
+        property must beEqualTo(Some(List("apples", "bananas", "oranges")))
+      }
+    }
   }
 }
