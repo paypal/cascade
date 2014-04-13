cbe84313f045597aa6140c66bac97bc49d18a323
Taylor Leese <tleese@paypal.com>
2014-04-12 19:29:49 -0700

    cleanup imports

--- common/src/main/scala/com/paypal/stingray/common/json/JsonUtil.scala
+++ common/src/main/scala/com/paypal/stingray/common/json/JsonUtil.scala
@@ -1,13 +1,10 @@
 package com.paypal.stingray.common.json
 
-import com.fasterxml.jackson.databind.{JavaType, DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.fasterxml.jackson.module.scala._
 import scala.util.Try
-import com.fasterxml.jackson.module.scala.introspect.ScalaClassIntrospectorModule
-import com.fasterxml.jackson.module.scala.deser.{OptionDeserializerModule, UntypedObjectDeserializerModule}
-import com.google.common.cache.{CacheBuilder, LoadingCache}
-import com.fasterxml.jackson.annotation.JsonInclude
 
 /**
  * Created by awharris on 1/17/14.
@@ -26,7 +23,6 @@ import com.fasterxml.jackson.annotation.JsonInclude
  */
 object JsonUtil {
 
-  // TODO: convert Manifest patterns to use TypeTag, ClassTag when Jackson implements that
   private val mapper = new ObjectMapper() with ScalaObjectMapper
 
   mapper.registerModule(DefaultScalaModule)
