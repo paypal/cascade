a2b2a2ad6104ff39d1cd1d703c7522589f74974f
gyeh <gyeh@paypal.com>
2014-05-01 12:18:19 -0700

    handling for unhandled, unsupervised exception

--- common/src/main/scala/com/paypal/stingray/common/app/StingrayApp.scala
+++ common/src/main/scala/com/paypal/stingray/common/app/StingrayApp.scala
@@ -1,13 +1,34 @@
 package com.paypal.stingray.common.app
 
 import org.slf4j.bridge.SLF4JBridgeHandler
-import org.slf4j.MDC
+import org.slf4j.{LoggerFactory, MDC}
+import ch.qos.logback.classic.LoggerContext
 
 /**
  * Starting point for runnable applications and services. Sets up logging and MDC values.
  * Otherwise functions like [[scala.App]].
  */
 trait StingrayApp extends App {
+
+  // properly kill app for unhandled, unsupervised exceptions
+  Thread.currentThread.setUncaughtExceptionHandler(
+    new Thread.UncaughtExceptionHandler() {
+      def uncaughtException(thread: Thread, cause: Throwable): Unit = {
+        val errMsg = s"Uncaught error from thread [${thread.getName}]. Shutting down JVM."
+
+        // print to both logs and console
+        val logger = LoggerFactory.getLogger(this.getClass)
+        logger.error(errMsg, cause)
+        System.err.println(errMsg)
+        cause.printStackTrace(System.err)
+
+        // flush logger and exit
+        val factory = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+        factory.stop()
+        System.exit(-1)
+      }
+    }
+  )
 
   /** The name of this service */
   val serviceName: String
