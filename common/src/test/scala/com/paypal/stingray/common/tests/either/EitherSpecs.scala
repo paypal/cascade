8bd1c8d0d68a17b0f1ce2fbfd7ce3c73c0a63b15
Matt Vaznaian <mvaznaian@paypal.com>
2014-05-08 11:08:30 -0700

    Common module at 87% coverage

---
+++ common/src/test/scala/com/paypal/stingray/common/tests/either/EitherSpecs.scala
@@ -0,0 +1,70 @@
+package com.paypal.stingray.common.tests.either
+
+import org.specs2._
+import com.paypal.stingray.common.either._
+import org.scalacheck.Prop._
+import scala.Exception
+import org.scalacheck.Arbitrary._
+
+/**
+ * Tests implicit classes in [[com.paypal.stingray.common.either]]
+ */
+class EitherSpecs extends Specification with ScalaCheck { def is =s2"""
+
+  AnyEitherToTry is an implicit wrapper to convert Either[E, A] to Try[A]
+
+  toTry, given a conversion function E => Exception
+    on a Left, return a Try Failure containing the converted Left exception ${AnyEitherToLeft().fails}
+    on a Right, return a Try Success containing the Right value             ${AnyEitherToRight().ok}
+
+  EitherOps is an implicit wrapper to convert regular objects to [[scala.util.Either]]
+
+  toRight creates an either with the object as the right      ${EitherOpsToRight().success}
+  toLeft creates an either with the object as the left        ${EitherOpsToLeft().success}
+
+  ThrowableEitherToTry is an implicit wrapper to convert Either[Throwable, A] to Try[A]
+
+  toTry should
+    on a Left, return a Try Failure containing the Left exception ${ThrowableEitherToLeft().fails}
+    on a Right, return a Try Success containing the Right value   ${ThrowableEitherToRight().ok}
+
+  """
+
+  private case class CustomException(m: String) extends Exception(m)
+
+  case class AnyEitherToLeft() {
+    def fails = forAll(arbitrary[Int]) { i =>
+      Left(i).toTry(intV => CustomException(intV.toString)) must beFailedTry.withThrowable[CustomException](i.toString)
+    }
+  }
+  case class AnyEitherToRight() {
+    def ok = forAll(arbitrary[String]) { s =>
+      (Right(s): Either[Int, String]).toTry(intV => CustomException(intV.toString)) must beSuccessfulTry.withValue(s)
+    }
+  }
+
+  case class EitherOpsToRight() {
+    def success = {
+      "hello".toRight[Throwable] must beAnInstanceOf[Either[Throwable, String]]
+    }
+  }
+
+  case class EitherOpsToLeft() {
+    def success = {
+      "hello".toLeft[Int] must beAnInstanceOf[Either[String, Int]]
+    }
+  }
+
+  case class ThrowableEitherToLeft() {
+    def fails = forAll(arbitrary[String]) { s =>
+      Left(CustomException(s)).toTry must beFailedTry.withThrowable[CustomException]
+    }
+  }
+
+  case class ThrowableEitherToRight() {
+    def ok = forAll(arbitrary[String]) { s =>
+      Right(s).toTry must beSuccessfulTry.withValue(s)
+    }
+  }
+
+}
--- common/src/test/scala/com/paypal/stingray/common/tests/enumeration/EnumerationSpecs.scala
+++ common/src/test/scala/com/paypal/stingray/common/tests/enumeration/EnumerationSpecs.scala
@@ -12,41 +12,81 @@ class EnumerationSpecs extends Specification { override def is = s2"""
 
   Tests our own type-safe Enumeration framework
 
-  readEnum should return Some if the string is a valid enum           ${readEnum().returnsSome}
-  readEnum should return None if the string is an invalid enum        ${readEnum().returnsNone}
-  toEnum should return the enum value if the string is a valid enum   ${toEnum().returns}
-  toEnum should throw if the string is an invalid enum value          ${toEnum().throws}
+  LowerEnumReader:
+
+  readEnum should return Some if the string is a valid enum           ${LowerReadEnum().returnsSome}
+  readEnum should return None if the string is an invalid enum        ${LowerReadEnum().returnsNone}
+  toEnum should return the enum value if the string is a valid enum   ${LowerToEnum().returns}
+  toEnum should throw if the string is an invalid enum value          ${LowerToEnum().throws}
+
+  UpperEnumReader:
+
+  readEnum should return Some if the string is a valid enum           ${UpperReadEnum().returnsSome}
+  readEnum should return None if the string is an invalid enum        ${UpperReadEnum().returnsNone}
+  toEnum should return the enum value if the string is a valid enum   ${UpperToEnum().returns}
+  toEnum should throw if the string is an invalid enum value          ${UpperToEnum().throws}
 
   """
 
-  class Context extends CommonImmutableSpecificationContext {
-    sealed abstract class MyEnum extends Enumeration
-    object MyEnum1 extends MyEnum {
+  class LowerContext extends CommonImmutableSpecificationContext {
+    sealed abstract class MyLowerEnum extends Enumeration
+    object MyLowerEnum1 extends MyLowerEnum {
       override lazy val stringVal = "myenum1"
     }
-    object MyEnum2 extends MyEnum {
+    object MyLowerEnum2 extends MyLowerEnum {
       override lazy val stringVal = "myenum2"
     }
-    implicit val reader = lowerEnumReader(MyEnum1, MyEnum2)
+    implicit val reader = lowerEnumReader(MyLowerEnum1, MyLowerEnum2)
   }
 
-  case class readEnum() extends Context {
+  class UpperContext extends CommonImmutableSpecificationContext {
+    sealed abstract class MyUpperEnum extends Enumeration
+    object MyUpperEnum1 extends MyUpperEnum {
+      override lazy val stringVal = "MYENUM1"
+    }
+    object MyUpperEnum2 extends MyUpperEnum {
+      override lazy val stringVal = "MYENUM2"
+    }
+    implicit val reader = upperEnumReader(MyUpperEnum1, MyUpperEnum2)
+  }
+
+  case class LowerReadEnum() extends LowerContext {
     def returnsSome = apply {
-      MyEnum1.stringVal.readEnum[MyEnum] must beSome.like {
-        case e => e must beEqualTo(MyEnum1)
+      MyLowerEnum1.stringVal.readEnum[MyLowerEnum] must beSome.like {
+        case e => e must beEqualTo(MyLowerEnum1)
       }
     }
     def returnsNone = apply {
-      s"${MyEnum1.stringVal}-INVALID".readEnum[MyEnum] must beNone
+      s"${MyLowerEnum1.stringVal}-INVALID".readEnum[MyLowerEnum] must beNone
     }
   }
 
-  case class toEnum() extends Context {
+  case class LowerToEnum() extends LowerContext {
     def returns = apply {
-      MyEnum1.stringVal.toEnum[MyEnum] must beEqualTo(MyEnum1)
+      MyLowerEnum1.stringVal.toEnum[MyLowerEnum] must beEqualTo(MyLowerEnum1)
     }
     def throws = apply {
-      Try(s"${MyEnum1.stringVal}-INVALID".toEnum[MyEnum]).toOption must beNone
+      Try(s"${MyLowerEnum1.stringVal}-INVALID".toEnum[MyLowerEnum]).toOption must beNone
+    }
+  }
+
+  case class UpperReadEnum() extends UpperContext {
+    def returnsSome = apply {
+      MyUpperEnum1.stringVal.readEnum[MyUpperEnum] must beSome.like {
+        case e => e must beEqualTo(MyUpperEnum1)
+      }
+    }
+    def returnsNone = apply {
+      s"${MyUpperEnum1.stringVal}-INVALID".readEnum[MyUpperEnum] must beNone
+    }
+  }
+
+  case class UpperToEnum() extends UpperContext {
+    def returns = apply {
+      MyUpperEnum1.stringVal.toEnum[MyUpperEnum] must beEqualTo(MyUpperEnum1)
+    }
+    def throws = apply {
+      Try(s"${MyUpperEnum1.stringVal}-INVALID".toEnum[MyUpperEnum]).toOption must beNone
     }
   }
 
