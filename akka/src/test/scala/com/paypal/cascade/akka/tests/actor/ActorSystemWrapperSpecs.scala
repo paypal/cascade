b3e706e0b4762e13dea82833fa7deb968b245665
Aaron Schlesinger <aschlesinger@paypal.com>
2014-10-01 13:06:59 -0700

    ActorSystemComponentSpecs => ActorSystemWrapperSpecs

--- akka/src/test/scala/com/paypal/cascade/akka/tests/actor/ActorSystemWrapperSpecs.scala
+++ akka/src/test/scala/com/paypal/cascade/akka/tests/actor/ActorSystemWrapperSpecs.scala
@@ -17,53 +17,48 @@ package com.paypal.cascade.akka.tests.actor
 
 import org.specs2._
 import org.specs2.mock.Mockito
-import com.paypal.cascade.akka.actor.ActorSystemComponent
-import com.paypal.cascade.common.service.ServiceNameComponent
+import com.paypal.cascade.akka.actor.ActorSystemWrapper
 import com.paypal.cascade.common.tests.util.CommonImmutableSpecificationContext
 
 /**
- * Tests for [[com.paypal.cascade.akka.actor.ActorSystemComponent]]
+ * Tests for [[com.paypal.cascade.akka.actor.ActorSystemWrapper]]
  */
-class ActorSystemComponentSpecs
+class ActorSystemWrapperSpecs
   extends SpecificationLike
   with ScalaCheck
   with Mockito { override def is = s2"""
 
-  Initializing a class which extends ActorSystemComponent should
+  Initializing an ActorSystemWrapper should
     Provide an actor system                  ${Initialize.ActorSystem().ok}
     Provide an ActorRefFactory               ${Initialize.ActorRefFactory().ok}
     Provide an execution context             ${Initialize.ExecutionContext().ok}
 
   """
 
-  class TestActorSystem extends ActorSystemComponent with ServiceNameComponent {
-    override lazy val serviceName = "akka"
-  }
-
-  trait Context
-    extends CommonImmutableSpecificationContext {
-    val actorSystem = new TestActorSystem
+  trait Context extends CommonImmutableSpecificationContext {
+    val serviceName = "testService"
+    val wrapper = new ActorSystemWrapper(serviceName)
   }
 
   object Initialize {
     case class ActorSystem() extends Context {
       def ok = this {
-        (actorSystem.system must not beNull) and
-          (actorSystem.system must beAnInstanceOf[akka.actor.ActorSystem])
+        (wrapper.system must not beNull) and
+          (wrapper.system must beAnInstanceOf[akka.actor.ActorSystem])
       }
     }
     case class ActorRefFactory() extends Context {
       def ok = this {
-        (actorSystem.actorRefFactory must not beNull) and
-          (actorSystem.actorRefFactory must beAnInstanceOf[akka.actor.ActorRefFactory]) and
-          (actorSystem.actorRefFactory must beEqualTo(actorSystem.system))
+        (wrapper.actorRefFactory must not beNull) and
+          (wrapper.actorRefFactory must beAnInstanceOf[akka.actor.ActorRefFactory]) and
+          (wrapper.actorRefFactory must beEqualTo(wrapper.system))
       }
     }
     case class ExecutionContext() extends Context {
       def ok = this {
-        (actorSystem.ec must not beNull) and
-          (actorSystem.ec must beAnInstanceOf[scala.concurrent.ExecutionContext]) and
-          (actorSystem.ec must beEqualTo(actorSystem.system.dispatcher))
+        (wrapper.executionContext must not beNull) and
+          (wrapper.executionContext must beAnInstanceOf[scala.concurrent.ExecutionContext]) and
+          (wrapper.executionContext must beEqualTo(wrapper.system.dispatcher))
       }
     }
   }
